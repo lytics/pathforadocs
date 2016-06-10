27f7c75acab53bee7e5169a78ac927cd73d309ad
Sarah Yablok <contact@sarahyablok.com>
2016-06-10 15:50:34 -0800

    Deployed c9c7086 with MkDocs version: 0.15.3

--- examples/src/layouts/slideout/positions.js
+++ examples/src/layouts/slideout/positions.js
@@ -2,14 +2,14 @@ var moduleLeft = pathfora.Subscription({
   id: 'slideout-left',
   layout: 'slideout',
   position: 'bottom-left',
-  headline: 'Left Aligned',
+  headline: 'Left Aligned'
 });
 
 var moduleRight = pathfora.Form({
   id: 'slideout-right',
   layout: 'slideout',
   position: 'bottom-right',
-  headline: 'Right Aligned',
+  headline: 'Right Aligned'
 });
 
-window.pathfora.initializeWidgets([ moduleLeft, moduleRight ]);
+pathfora.initializeWidgets([ moduleLeft, moduleRight ]);
